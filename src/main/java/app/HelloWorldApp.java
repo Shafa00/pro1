8213a75a74f38529772c431ba55daea038ad3ac1
Shafa <smammadova11@gmail.com>
2020-02-10 18:59:59 +0400

    calculator

--- src/main/java/app/HelloWorldApp.java
+++ src/main/java/app/HelloWorldApp.java
@@ -1,9 +1,19 @@
 package app;
-
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 public class HelloWorldApp {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    PrintStream output=System.out;
+    output.print("Input first number: ");
+    InputStream in = System.in;
+    Scanner scan = new Scanner(in);
+    int x = scan.nextInt();
+    output.print("Input second number: ");
+    int y=scan.nextInt();
+    int sum=x+y;
+    output.print("Sum of two numbers: "+ sum);
   }
 
 }
