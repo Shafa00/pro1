d8f7a1b44096d60ad9afe3279c6d128682099bbd
Shafa <smammadova11@gmail.com>
2020-05-18 02:28:09 +0400

    hackerrank

---
+++ src/main/java/hackerrank/ElectronicShop.java
@@ -0,0 +1,25 @@
+package hackerrank;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class ElectronicShop {
+    static int getMoneySpent(int[] keyboards, int[] drives, int b) {
+        int max = -1;
+        for (int keyboard : keyboards) {
+            for (int drive : drives) {
+                int sum = keyboard + drive;
+                if (sum <= b && sum > max) {
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] keyboards = {3 ,1};
+        int[] drives = {5 ,2 ,8};
+        System.out.println(getMoneySpent(keyboards, drives, 10));
+    }
+}
---
+++ src/main/java/hackerrank/SockMerchant.java
@@ -0,0 +1,12 @@
+package hackerrank;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SockMerchant {
+
+
+    public static void main(String[] args) {
+        int [] ar = {10,20,20,10,10,30,50,10,20};
+    }
+}
